19b9806838f1adc3856e0c5b70663a19018e510f
wswidzinski <swidzinskiw@gmail.com>
2018-02-02 19:47:05 +0100

    Add basic styling to OrderForm, add web3js

--- src/components/OrderForm/index.jsx
+++ src/components/OrderForm/index.jsx
@@ -9,12 +9,15 @@ import 'preact-material-components/Menu/style.css';
 import 'preact-material-components/Select/style.css';
 import 'preact-material-components/Button/style.css';
 import 'preact-material-components/Theme/style.css';
+import style from './style.scss';
 
 export default class OrderForm extends Component {
   render(){
     return (
-      <div>
-        <Select hintText="Select restaurant"
+      <div class={style.form}>
+        <Select
+          className={style['form-input']}
+          hintText="Select restaurant"
           selectedIndex={this.state.chosenIndex}
           onChange={(e)=>{
             this.setState({
@@ -26,12 +29,8 @@ export default class OrderForm extends Component {
             <Select.Item>Ether Pizza</Select.Item>
             <Select.Item>Ether Burger - coming soon</Select.Item>
           </Select>
-          <div>
-            <TextField label="Address"/>
-          </div>
-          <div>
-            <TextField type="tel" label="Phone number"/>
-          </div>
+          <TextField className={style['form-input']} label="Address"/>
+          <TextField className={style['form-input']} type="tel" label="Phone number"/>
           <div>
             <Button raised>Create order</Button>
           </div>
